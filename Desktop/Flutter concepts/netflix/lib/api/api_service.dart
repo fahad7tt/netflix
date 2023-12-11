21e2598b20dbaf03d2b3fcd4ae4b6a122c76cba1
Fahad TT <ttfahad2317@gmail.com>
2023-12-11 09:26:19 +0530

    Initial commit

---
+++ Desktop/Flutter concepts/netflix/lib/api/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'constants.dart';
+import 'model_json.dart';
+
+class MovieService{
+
+      Future<List<ApiDataModel>> getMovies(String category) async{
+
+    final url = Uri.parse('${ApiConfig.baseUrl}$category${ApiConfig.apiKey}');
+    final response = await http.get(url);
+    if(response.statusCode == 200){
+      final data = json.decode(response.body);
+      final List<dynamic> results = data['results'];
+      return results.map((json) => ApiDataModel.fromJson(json)).toList();
+    }else{
+      throw Exception('Could Not Fetch Movies');
+    }
+  }
+}
